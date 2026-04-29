2a65b19634e60e4ca0dda0699dfbe0fd7c9287d8
Dorian117 <sh02280301@gmail.com>
2026-04-28 22:24:34 -0500

    DeleateTestofGetPostman:D

--- src/app.js
+++ src/app.js
@@ -13,17 +13,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'MagicRain API v1.0', status: 'online' });
 });
 
-app.get('/test-models', (req, res) => {
-  try {
-    require('./models/User');
-    require('./models/Node');
-    require('./models/Reading');
-    res.json({ models: { User: 'cargado', Node: 'cargado', Reading: 'cargado' } });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
 // app.use('/api/auth', authRoutes);
 // app.use('/api/sensors', sensorRoutes);
 
